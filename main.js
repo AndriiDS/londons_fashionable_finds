83cb62aa16878299706ab74763b493c09056aa60
Andrii Havryliuk <andrii.havryliuk@gmail.com>
2023-04-26 13:04:31 +0100

    add static folder

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetch('data/ranked_shops.json')
-        .then(response => response.json())
-        .then(data => {
-            populateTable(data);
-        })
-        .catch(error => {
-            console.error('Error fetching JSON data:', error);
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("./static/ranked_shops.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const tableBody = document.getElementById("table-body");
+  
+        data.forEach((shop, index) => {
+          const row = document.createElement("tr");
+  
+          const rankCell = document.createElement("td");
+          rankCell.textContent = index + 1;
+          row.appendChild(rankCell);
+  
+          const nameCell = document.createElement("td");
+          nameCell.textContent = shop.Shop_Name;
+          row.appendChild(nameCell);
+  
+          const addressCell = document.createElement("td");
+          addressCell.textContent = shop.Address;
+          row.appendChild(addressCell);
+  
+          const scoreCell = document.createElement("td");
+          scoreCell.textContent = parseFloat(shop.composite_score).toFixed(2);
+          row.appendChild(scoreCell);
+  
+          tableBody.appendChild(row);
         });
-});
-
-function populateTable(data) {
-    const tableBody = document.getElementById('table-body');
-
-    data.forEach((shop, index) => {
-        const row = document.createElement('tr');
-
-        const rankCell = document.createElement('td');
-        rankCell.textContent = index + 1;
-        row.appendChild(rankCell);
-
-        const shopNameCell = document.createElement('td');
-        shopNameCell.textContent = shop.Shop_Name;
-        row.appendChild(shopNameCell);
-
-        const addressCell = document.createElement('td');
-        addressCell.textContent = shop.Address;
-        row.appendChild(addressCell);
-
-        const compositeScoreCell = document.createElement('td');
-        compositeScoreCell.textContent = parseFloat(shop.composite_score).toFixed(2);
-        row.appendChild(compositeScoreCell);
-
-        tableBody.appendChild(row);
-    });
-}
+      })
+      .catch((error) => console.error("Error fetching JSON data:", error));
+  });
+  
