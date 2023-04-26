118a75dd24dbd331636a5e00137b139c6218b67a
Andrii Havryliuk <andrii.havryliuk@gmail.com>
2023-04-26 22:39:00 +0100

    fix webpage

--- main.js
+++ main.js
@@ -1,31 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("./static/ranked_shops.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const tableBody = document.getElementById("table-body");
-  
-        data.forEach((shop, index) => {
-          const row = document.createElement("tr");
-  
-          const rankCell = document.createElement("td");
-          rankCell.textContent = index + 1;
-          row.appendChild(rankCell);
-  
-          const nameCell = document.createElement("td");
-          nameCell.textContent = shop.Shop_Name;
-          row.appendChild(nameCell);
-  
-          const addressCell = document.createElement("td");
-          addressCell.textContent = shop.Address;
-          row.appendChild(addressCell);
-  
-          const scoreCell = document.createElement("td");
-          scoreCell.textContent = parseFloat(shop.composite_score).toFixed(2);
-          row.appendChild(scoreCell);
-  
-          tableBody.appendChild(row);
-        });
-      })
-      .catch((error) => console.error("Error fetching JSON data:", error));
+fetch("static/ranked_shops.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let tableBody = document.querySelector("tbody");
+    data.forEach((shop, index) => {
+      let row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${shop.name}</td>
+        <td>${shop.address}</td>
+        <td>${shop.composite_score.toFixed(2)}</td>
+      `;
+      tableBody.appendChild(row);
+    });
   });
-  
