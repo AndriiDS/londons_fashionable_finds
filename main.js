f5620ab8efef53e2294de6b2694aa2b8502c94fb
Andrii Havryliuk <andrii.havryliuk@gmail.com>
2023-04-26 12:59:09 +0100

    Fix json and html

--- main.js
+++ main.js
@@ -1,27 +1,36 @@
-fetch("ranked_shops.json")
-  .then((response) => response.json())
-  .then((shops) => {
-    const tableBody = document.querySelector("tbody");
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('data/ranked_shops.json')
+        .then(response => response.json())
+        .then(data => {
+            populateTable(data);
+        })
+        .catch(error => {
+            console.error('Error fetching JSON data:', error);
+        });
+});
 
-    shops.forEach((shop, index) => {
-      const row = document.createElement("tr");
+function populateTable(data) {
+    const tableBody = document.getElementById('table-body');
 
-      const rankCell = document.createElement("td");
-      rankCell.textContent = index + 1;
-      row.appendChild(rankCell);
+    data.forEach((shop, index) => {
+        const row = document.createElement('tr');
 
-      const shopNameCell = document.createElement("td");
-      shopNameCell.textContent = shop.Shop_Name;
-      row.appendChild(shopNameCell);
+        const rankCell = document.createElement('td');
+        rankCell.textContent = index + 1;
+        row.appendChild(rankCell);
 
-      const addressCell = document.createElement("td");
-      addressCell.textContent = shop.Address;
-      row.appendChild(addressCell);
+        const shopNameCell = document.createElement('td');
+        shopNameCell.textContent = shop.Shop_Name;
+        row.appendChild(shopNameCell);
 
-      const scoreCell = document.createElement("td");
-      scoreCell.textContent = parseFloat(shop.composite_score).toFixed(2);
-      row.appendChild(scoreCell);
+        const addressCell = document.createElement('td');
+        addressCell.textContent = shop.Address;
+        row.appendChild(addressCell);
 
-      tableBody.appendChild(row);
+        const compositeScoreCell = document.createElement('td');
+        compositeScoreCell.textContent = parseFloat(shop.composite_score).toFixed(2);
+        row.appendChild(compositeScoreCell);
+
+        tableBody.appendChild(row);
     });
-  });
+}
