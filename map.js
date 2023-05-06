1e7735090d517dc8c3bfa95b95060f9da241aee7
Andrii Havryliuk <andrii.havryliuk@gmail.com>
2023-05-06 11:13:52 +0100

    map fix

---
+++ map.js
@@ -0,0 +1,24 @@
+<script>
+  $(document).ready(function () {
+    var map = L.map("map").setView([51.5074, -0.1278], 10);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    $.getJSON("./static/top_20_shops_by_borough.json", function (data) {
+      data.forEach(function (shop) {
+        var marker = L.marker([shop.Latitude, shop.Longitude]).addTo(map);
+        marker.bindPopup(
+          "<b>" +
+            shop.Shop_Name +
+            "</b><br>Rating: " +
+            shop.Rating +
+            "<br>Address: " +
+            shop.Address
+        );
+      });
+    });
+  });
+</script>
