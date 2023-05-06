e05a1656350b7e5bccb85592ed8d9d1447eac1bb
Andrii Havryliuk <andrii.havryliuk@gmail.com>
2023-05-06 11:19:45 +0100

    map shops fix

--- map.js
+++ map.js
@@ -1,13 +1,12 @@
-<script>
-  $(document).ready(function () {
+$(document).ready(function () {
     var map = L.map("map").setView([51.5074, -0.1278], 10);
-
+  
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-
-    $.getJSON("./static/top_20_shops_by_borough.json", function (data) {
+  
+    $.getJSON("./static/ranked_shops.json", function (data) {
       data.forEach(function (shop) {
         var marker = L.marker([shop.Latitude, shop.Longitude]).addTo(map);
         marker.bindPopup(
@@ -21,4 +20,4 @@
       });
     });
   });
-</script>
+  
